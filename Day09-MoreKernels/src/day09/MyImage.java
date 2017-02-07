82914e4ca6f299b1a3fcfea4ee3b6fa4c9ada8dc
B. Ricks <bricks@unomaha.edu>
2017-02-07 15:47:24 -0600

    Adding Day 09

---
+++ Day09-MoreKernels/src/day09/MyImage.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day09;
+
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author unouser
+ */
+public class MyImage extends WritableImage{
+
+    public MyImage(int i, int i0) {
+        super(i, i0);
+    }
+    
+    public void copyFrom(Image inImage){
+        for(int y = 0; y < this.getHeight() ;  y++)
+        {
+            for(int x = 0; x < this.getWidth() ; x++)
+            {
+                this.getPixelWriter().setColor(x, y,  inImage.getPixelReader().getColor(x, y));
+            }
+        }
+    }
+    
+    public void save(){
+        RenderedImage renderedImage = SwingFXUtils.fromFXImage(this, null);
+        try {
+            ImageIO.write(renderedImage, "PNG", new File(System.getProperty("user.home") + "/Desktop/newPhoto.png") );
+        } catch (IOException ex) {
+            Logger.getLogger(MyImage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    private void applyKernel(float[][] kernel, int w)
+    {
+        Color[][] newColors = new Color[(int)this.getHeight()][(int)this.getWidth()];
+        
+        for(int y = 0; y < this.getHeight() ;  y++)
+        {
+            for(int x = 0; x < this.getWidth() ; x++)
+            {
+                
+                Color myColor = this.getPixelReader().getColor(x, y );
+                if(myColor.getOpacity() == 0)
+                {
+                    newColors[y][x] = Color.TRANSPARENT;
+                    continue;
+                }
+                
+                
+                float sumR= 0;
+                float sumG= 0;
+                float sumB= 0;
+                
+                float power = 0;
+                
+                for(int i = -1 * w; i <= 1 * w; i++)
+                {
+                    for(int j = -1 * w; j <= 1 * w; j++)
+                    {
+                        if(this.doesPixelExist(x + i, y + j))
+                        {
+                            Color currentPixel = this.getPixelReader().getColor(x + i, y + j);
+                            float kernelValue = kernel[j + w][i + w];
+                            sumR += currentPixel.getRed() * kernelValue;
+                            sumG += currentPixel.getGreen()* kernelValue;
+                            sumB += currentPixel.getBlue()* kernelValue;
+                            power += kernelValue;
+                        }
+                        
+                    }
+                }
+                
+                sumR /= power;
+                sumG /= power;
+                sumB /= power;
+                
+                newColors[y][x] = new Color(sumR, sumG, sumB, 1.0f );
+            }
+        }
+        
+        for(int y = 0; y < this.getHeight() ;  y++)
+        {
+            for(int x = 0; x < this.getWidth() ; x++)
+            {
+                this.getPixelWriter().setColor(x, y, newColors[y][x]);
+            }
+        }
+    }
+    
+    public void blur(int w) {
+        
+        float[][] blurKernel = new float[2*w + 1][2 * w + 1];
+        
+        for(int y = 0; y < 2 * w + 1 ;  y++)
+        {
+            for(int x = 0; x < 2 * w+ 1 ; x++)
+            {
+                blurKernel[y][x] = 1;
+            }
+        }
+        
+        this.applyKernel(blurKernel, w);
+        
+    }
+
+    private boolean doesPixelExist(int x, int y) {
+        return x < this.getWidth() && x >= 0 && y < this.getHeight() && y >= 0;
+    }
+    
+    
+    
+}
